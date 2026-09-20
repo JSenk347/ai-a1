acc31ee67c84480d88fe020fac64d614a11f501f
Joseph <millsjd@telus.net>
2026-09-20 14:57:45 -0600

    completed class scorer, defines itself, callig, and setting best score

--- scoring.py
+++ scoring.py
@@ -1,3 +1,29 @@
+class Scorer:
+
+    def __init__(self, translator):
+
+        self.translator = translator     
+        self.history = []   # pair of (knobs_setting, score)
+        
+    def __call__(self, knobs_setting):
+
+        knobs_setting = self.check_settings(knobs_setting)
+
+        message = self.translator.translate(knobs_setting)
+        score = decode(message)
+
+        self.history.append((knobs_setting, score))
+
+        return score
+    
+    def best(self):
+        best_settings, best_score = self.history[0]
+        
+        for settings, score in self.history:
+            if score > best_score:
+                best_settings = settings
+                best_score = score
+        return best_settings, best_score
 
 
 def decode(message):
@@ -13,3 +39,5 @@ def decode(message):
         translated_count = translated_count + 1
 
     return translated_count / len(words)
+
+def check_settings()
