012737ba0316d9f57cdc2fa97380efd3b551955d
Darren <darren.lyl@gmail.com>
2026-09-22 15:52:10 -0600

    added 60 second timer

--- part2.py
+++ part2.py
@@ -1,6 +1,6 @@
 from translator import UniversalTranslator
 import numpy as np
-
+import time
 
 # create the UniversalTranslator object, with 10 knobs
 translator = UniversalTranslator(n_dim=10)
@@ -12,3 +12,12 @@ print(translated_string)
 
 # print total number of settings evaluated
 print(f'# settings tried: {translator.n_settings_tried()}')
+
+# 60 second timer
+def sixty_seconds():
+    for seconds in range(60, -1, -1):
+        print(f"\rTime remaining: {seconds} seconds", end="", flush=True)
+        time.sleep(1)
+
+    print("\nTime's up!")
+
